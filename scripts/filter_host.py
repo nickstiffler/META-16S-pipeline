6fe88cf0a2add0ea18b805312f135c57b4f73b25
Nicholas Stiffler <stiffler@teosinte.uoregon.edu>
2015-09-30 17:43:35 -0700

    Cannot test filter_host until merged table is added.

--- scripts/filter_host.py
+++ scripts/filter_host.py
@@ -4,7 +4,7 @@
 
 # John Conery / Kevin Xu Junjie / Nicholas Stiffler
 # University of Oregon
-# 2015-0924
+# 2015-09-24
 
 import sqlite3
 import argparse
@@ -18,14 +18,15 @@ from common import *
 # Define filenames (may be referenced by more than one function)
 
 result_file = 'output.sam'
-input_file = 'clusters.fasta'
+input_file = 'merged.fasta'
 
 ###
 # Print the sequences to check; they're the centroids in the clusters saved in the clusters table
 
+# Should it check for a "merged" folder and use those sequence instead to save time?
 
 def print_sequences(db, args):
-    fetch_clusters = 'SELECT name, sequence FROM clusters'
+    fetch_clusters = 'SELECT defline, sequence FROM merged WHERE filtered = 0'
     record_metadata(db, 'query', fetch_clusters)
     ff = open(os.path.join(args.workspace, input_file), 'w')
     for name, sequence in db.execute(fetch_clusters):
@@ -37,38 +38,48 @@ def print_sequences(db, args):
 # Run the app
 
 def run_bowtie(args):
-    cmnd = 'bowtie2 -p ' + os.cpu_count()
-    cmnd += ' -uchimeout ' + os.path.join(args.workspace, result_file)
+    cmnd = 'bowtie2 -p {} '.format(os.cpu_count())
+    cmnd += ' --no-unal ' # Suppress unaligned reads from SAM file
+    cmnd += ' -f ' # Fasta as input instead of fatsq
+    cmnd += ' --no-hd ' # Suppress header
+    cmnd += ' -S ' + os.path.join(args.workspace, result_file)
     cmnd += ' -x ' + args.reference
-    cmnd += os.path.join(args.workspace, input_file)
-    cmnd += ' > /dev/null'
+    cmnd += ' -U ' + os.path.join(args.workspace, input_file)
     print(cmnd)
     record_metadata(db, 'exec', cmnd, commit=True)
     res = os.system(cmnd)
     
 ###
-# Parse the output file, update cluster table  sequences in a new table.
+# Parse the output file, update merged table to indicate the sequence matches host
+# Use "2" for filtered column
 
-insert_record = 'INSERT INTO chimeras (cluster_id, chimeric) VALUES (?,?)'
+update_record = 'UPDATE merged (merged_id, filtered) VALUES (?,2)'
 
 def import_results(db, args):
-    cmap = make_cmap(db)
+    #mmap = make_mmap(db)
     for line in open(os.path.join(args.workspace, result_file)):
         res = line.split('\t')
-        defspec = res[1]
-        chimeric = res[-1].strip()
-        db.execute(insert_record, (cmap[defspec], chimeric))
+        defspec = res[0]
+        #host = res[-1].strip()
+        #db.execute(update_record, (mmap[defspec]))
+        print (merged_from_defline(defspec))
 
-def make_cmap(db):
-    cmap = { }
-    for cid, name in db.execute('SELECT cluster_id, name FROM clusters'):
-        cmap[name] = cid
-    return cmap
+# Is this nessecary or can we just have merged_id in the FASTA and skip this step?
+# Found this step takes too long. Replacing with individual queries
+def make_mmap(db):
+    mmap = { }
+    for mid, name in db.execute('SELECT merged_id, defline FROM merged'):
+        mmap[name] = mid
+    return mmap
+
+def merged_from_defline(defline):
+    if db.execute('SELECT merged_id, defline FROM merge'):
+        return db.fetchone()[0]
 
 ###
 # Top level function: initialize the workspace directory, run the app
 
-def filter_chimeras(db, args):
+def filter_host(db, args):
     init_workspace(args)
     print_sequences(db, args)
     run_bowtie(args)
@@ -90,12 +101,12 @@ if __name__ == "__main__":
     db = sqlite3.connect(args.dbname)
     record_metadata(db, 'start', ' '.join(sys.argv[1:]))
 
-    try:
-        chimera_spec = [('cluster_id', 'foreign', 'clusters'), ('chimeric', 'CHAR(1)')]
-        init_table(db, 'chimeras', 'chimera_id', chimera_spec, args.force)
-    except Exception as err:
-        print('Error while initializing output tables:', err)
-        argparse.ArgumentParser.exit(1, 'Script aborted')
+    #try:
+    #    chimera_spec = [('cluster_id', 'foreign', 'clusters'), ('chimeric', 'CHAR(1)')]
+    #    init_table(db, 'chimeras', 'chimera_id', chimera_spec, args.force)
+    #except Exception as err:
+    #    print('Error while initializing output tables:', err)
+    #    argparse.ArgumentParser.exit(1, 'Script aborted')
 
     filter_host(db, args)
     record_metadata(db, 'end', '')
